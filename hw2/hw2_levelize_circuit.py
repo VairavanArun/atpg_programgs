53f5e86fe3a767a2d5c8c7340fcda3d28a199c2e
Vairavan <vairavan.arun13@gmail.com>
2024-09-17 23:27:42 -0500

    Updated to check for two circuits in one run

--- hw2/hw2_levelize_circuit.py
+++ hw2/hw2_levelize_circuit.py
@@ -1,5 +1,9 @@
 from atpg import circuit
-
-circuit_under_test = circuit("p2.txt")
-circuit_under_test.levelize_circuit()
-print(circuit_under_test)
+circuit_under_test_1 = circuit("p1.txt")
+circuit_under_test_1.levelize_circuit()
+circuit_under_test_2 = circuit("p2.txt")
+circuit_under_test_2.levelize_circuit()
+print("-----Circuit from bench file p1.txt-----")
+print(circuit_under_test_1)
+print("-----Circuit from bench file p2.txt-----")
+print(circuit_under_test_2)
